d68a496d581bc515c9f61de5adcd5ccde4a08c59
Luca Schmitz <50083110+LucaSchmitz2003@users.noreply.github.com>
2026-01-16 22:20:29 +0100

    Added a get id func with parsing built in

--- internal/dtos/token_claims.go
+++ internal/dtos/token_claims.go
@@ -1,8 +1,22 @@
 package dtos
 
-import "github.com/golang-jwt/jwt/v5"
+import (
+	"strconv"
+
+	"github.com/golang-jwt/jwt/v5"
+)
 
 type CustomTokenClaims struct {
 	jwt.RegisteredClaims
 	TokenType string `json:"token_type"`
+}
+
+func (claims *CustomTokenClaims) GetId() (uint, error) {
+	subject := claims.Subject
+	id, err := strconv.ParseUint(subject, 10, 0)
+	if err != nil {
+		return 0, err
+	}
+
+	return uint(id), nil
 }
