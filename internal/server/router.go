09d0de779b0a1483db600ea13f97eb96f30dadb5
Luca Schmitz <50083110+LucaSchmitz2003@users.noreply.github.com>
2026-01-16 22:06:49 +0100

    Added protected routes

--- internal/server/router.go
+++ internal/server/router.go
@@ -3,16 +3,27 @@ package server
 import (
 	"github.com/bestreads/Backend/internal/config"
 	"github.com/bestreads/Backend/internal/handlers"
+	"github.com/bestreads/Backend/internal/middlewares"
+	"github.com/bestreads/Backend/internal/types"
 	"github.com/gofiber/fiber/v2"
+	"github.com/rs/zerolog"
 )
 
-func setRoutes(cfg *config.Config, app *fiber.App) {
+func setRoutes(cfg *config.Config, log zerolog.Logger, app *fiber.App) {
+	// Create the main API base path group
 	basePath := app.Group(cfg.ApiBasePath)
+
+	// Initialize api version groups
 	v1 := basePath.Group("/v1")
+
+	// --- Public routes ---
 
 	auth := v1.Group("/auth")
 	auth.Post("/login", handlers.Login)
-	auth.Post("/refresh", handlers.TokenRefresh)
+	auth.Post("/refresh",
+		middlewares.Protected(cfg, log, types.RefreshToken),
+		handlers.TokenRefresh,
+	)
 	auth.Post("/logout", handlers.Logout)
 
 	v1.Get("/health", handlers.Health)
@@ -38,4 +49,11 @@ func setRoutes(cfg *config.Config, app *fiber.App) {
 	v1user.Post("/lib", handlers.AddToLibrary)
 	v1user.Put("/lib/:BID", handlers.UpdateReadingStatus)
 	v1user.Delete("/lib/:BID", handlers.DeleteFromLibrary)
+
+	// Apply the authentication middleware to a new sub-group
+	v1Protected := v1.Group("/", middlewares.Protected(cfg, log, types.AccessToken))
+
+	// --- Protected routes ---
+
+	v1Protected.Get("/test", handlers.Health)
 }
