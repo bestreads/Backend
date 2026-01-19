438f51847134c6a32be562772c37bad6dceb4322
Luca Schmitz <50083110+LucaSchmitz2003@users.noreply.github.com>
2026-01-19 17:38:03 +0100

    Protect all relevant endpoints

--- internal/server/router.go
+++ internal/server/router.go
@@ -18,6 +18,9 @@ func setRoutes(cfg *config.Config, log zerolog.Logger, app *fiber.App) {
 
 	// --- Public routes ---
 
+	v1user := v1.Group("/user")
+	v1user.Post("/", handlers.CreateUser)
+
 	auth := v1.Group("/auth")
 	auth.Post("/login", handlers.Login)
 	auth.Post("/refresh",
@@ -27,27 +30,16 @@ func setRoutes(cfg *config.Config, log zerolog.Logger, app *fiber.App) {
 	auth.Post("/logout", handlers.Logout)
 
 	v1.Get("/health", handlers.Health)
-	v1.Get("/books/search", handlers.BookSearch)
-	v1.Get("/books/:bid", handlers.GetBook)
-
-	// ?limit=n
-	v1.Get("/post", handlers.GetPost)
-
-	// ?type=0|1|2
-	v1.Put("/media", handlers.SaveFile)
-	// ?type=0|1|2
-	v1.Get("/media/:KEY", handlers.GetFile)
 
 	// Apply the authentication middleware to a new sub-group
 	v1Protected := v1.Group("/", middlewares.Protected(cfg, log, types.AccessToken))
 
 	// --- Protected routes ---
 
-	v1user := v1Protected.Group("/user")
-	v1user.Get("/", handlers.GetOwnUser)
-	v1user.Post("/", handlers.CreateUser)
-	v1user.Get("/profile/:id", handlers.GetUserProfile)
-	v1userWithId := v1user.Group("/:ID")
+	v1userProtected := v1Protected.Group("/user")
+	v1userProtected.Get("/", handlers.GetOwnUser)
+	v1userProtected.Get("/profile/:id", handlers.GetUserProfile)
+	v1userWithId := v1userProtected.Group("/:ID")
 	v1userWithId.Post("/post", handlers.CreatePost)
 
 	// ?limit=n
@@ -55,4 +47,17 @@ func setRoutes(cfg *config.Config, log zerolog.Logger, app *fiber.App) {
 	v1userWithId.Post("/lib", handlers.AddToLibrary)
 	v1userWithId.Put("/lib/:BID", handlers.UpdateReadingStatus)
 	v1userWithId.Delete("/lib/:BID", handlers.DeleteFromLibrary)
+
+	v1booksProtected := v1Protected.Group("/books")
+	v1booksProtected.Get("/search", handlers.BookSearch)
+	v1booksProtected.Get("/:bid", handlers.GetBook)
+
+	// ?limit=n
+	v1Protected.Get("/post", handlers.GetPost)
+
+	v1mediaProtected := v1Protected.Group("/media")
+	// ?type=0|1|2
+	v1mediaProtected.Put("/", handlers.SaveFile)
+	// ?type=0|1|2
+	v1mediaProtected.Get("/:KEY", handlers.GetFile)
 }
