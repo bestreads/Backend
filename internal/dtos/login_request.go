37002eb91870f8c021aa817dad4dc9e8be66b2b9
Luca Schmitz <50083110+LucaSchmitz2003@users.noreply.github.com>
2026-01-28 16:52:19 +0100

    Add swagger doc to login endpoint

--- internal/dtos/login_request.go
+++ internal/dtos/login_request.go
@@ -1,6 +1,6 @@
 package dtos
 
 type LoginRequest struct {
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=12"`
+	Email    string `json:"email" example:"test@bestreads.byte-flow.de" validate:"required,email"`
+	Password string `json:"password" example:"ichBinEinPasswort" validate:"required,min=12"`
 }
