47ab5e94b3b8d1b058ca586cf02ab32d9ef226a1
erik <erschmia@gmail.com>
2026-02-11 14:50:55 +0100

    mehr tests

--- testing/e2e.py
+++ testing/e2e.py
@@ -2,18 +2,53 @@ import http.client as cl
 import json
 import traceback
 import requests
+import datetime
+
+class Co:
+    """ ANSI color codes """
+    BLACK = "\033[0;30m"
+    RED = "\033[0;31m"
+    GREEN = "\033[0;32m"
+    BROWN = "\033[0;33m"
+    BLUE = "\033[0;34m"
+    PURPLE = "\033[0;35m"
+    CYAN = "\033[0;36m"
+    LIGHT_GRAY = "\033[0;37m"
+    DARK_GRAY = "\033[1;30m"
+    LIGHT_RED = "\033[1;31m"
+    LIGHT_GREEN = "\033[1;32m"
+    YELLOW = "\033[1;33m"
+    LIGHT_BLUE = "\033[1;34m"
+    LIGHT_PURPLE = "\033[1;35m"
+    LIGHT_CYAN = "\033[1;36m"
+    LIGHT_WHITE = "\033[1;37m"
+    BOLD = "\033[1m"
+    FAINT = "\033[2m"
+    ITALIC = "\033[3m"
+    UNDERLINE = "\033[4m"
+    BLINK = "\033[5m"
+    NEGATIVE = "\033[7m"
+    CROSSED = "\033[9m"
+    END = "\033[0m"
 
 HOST = "localhost:3000"
 API_PATH = "/api/v1"
+
+# login-test
 USERNAME = "test3@test.arpa"
 PASSWORD = "test1234567890"
+
+# follow-test
+FOLLOW_ID = 10
+GET_FOLLOW_TARGET = 10
 
 def healtcheck():
     resp = requests.get(url=f"http://{HOST}{API_PATH}/health")
 
     if resp.status_code != 200:
         raise Exception(f"failed the healthcheck: {resp.status_code}; body: {resp.text}")
-    return
+    else:
+        log(f"{resp.text}", 3)
 
 def login() -> List[dict]:
     resp = requests.post(url=f"http://{HOST}{API_PATH}/auth/login", json={"email": USERNAME, "password": PASSWORD})
@@ -26,48 +61,103 @@ def login() -> List[dict]:
     for h in resp.headers:
         if h == "Set-Cookie":
             tokens['Cookie']  = resp.headers[h]
-            
+    if len(tokens) == 0:
+        raise Exception("no tokens found :(")
+
+    log(f"tokens: {len(tokens)}", 3)
+
     return tokens
     
 def follow(tokens: dict[str]):
     headers = {"Host": HOST}
     headers['Cookie'] = tokens['Cookie']
 
-    resp = requests.post(url=f"http://{HOST}{API_PATH}/follow?id=10", headers=headers)
+    resp = requests.post(url=f"http://{HOST}{API_PATH}/follow?id={FOLLOW_ID}", headers=headers)
 
     if resp.status_code != 200:
         raise Exception(f"failed to follow user: {resp.status_code}; body: {resp.text}")
-    return
+    else:
+        log(f"{resp.text}", 3)
+
+def unfollow(tokens: dict[str]):
+    headers = {"Host": HOST}
+    headers['Cookie'] = tokens['Cookie']
+
+    resp = requests.delete(url=f"http://{HOST}{API_PATH}/follow?id={FOLLOW_ID}", headers=headers)
+
+    if resp.status_code != 200:
+        raise Exception(f"failed to follow user: {resp.status_code}; body: {resp.text}")
+    else:
+        log(f"{resp.text}", 3)
+
+def get_followers(tokens: dict[str]):
+    headers = {"Host": HOST}
+    headers['Cookie'] = tokens['Cookie']
+
+    resp = requests.get(url=f"http://{HOST}{API_PATH}/user/{GET_FOLLOW_TARGET}/followers", headers=headers)
+
+    if resp.status_code != 200:
+        raise Exception(f"failed to get follower list: {resp.status_code}; body: {resp.text}")
+    else:
+        log(f"{resp.text}", 3)
+
+def get_following(tokens: dict[str]):
+    headers = {"Host": HOST}
+    headers['Cookie'] = tokens['Cookie']
+
+    resp = requests.get(url=f"http://{HOST}{API_PATH}/user/{GET_FOLLOW_TARGET}/following", headers=headers)
+
+    if resp.status_code != 200:
+        raise Exception(f"failed to get follower list: {resp.status_code}; body: {resp.text}")
+    else:
+        log(f"{resp.text}", 3)
+
+    
 
 
+def log(msg: str, type: int):
+    now = datetime.datetime.now()
+    if type == 0:
+        # OK
+        print(f"{Co.END}{str(now)} | [{Co.LIGHT_GREEN}  OK  {Co.END}] {Co.END}{msg}")
+    elif type == 1:
+        # ERROR
+        print(f"{Co.END}{str(now)} | [{Co.RED} FAIL {Co.END}] {Co.END}{msg}")
 
-def green(s: str) -> str:
-    return f"\033[1;49;32m{s}\033[0m"
-
-def red(s: str) -> str:
-    return f"\033[1;49;91m{s}\033[0m"
-
-def yellow(s: str) -> str:
-    return f"\033[1;49;33m{s}\033[0m"
-
+    elif type == 2:
+        # running
+        print(f"{Co.END}{str(now)} | [{Co.LIGHT_CYAN} EXEC {Co.END}] {Co.END}{msg}")
+    elif type == 3:
+        print(f"{Co.END}{str(now)} | [{Co.LIGHT_PURPLE} INFO {Co.END}] {Co.END}{msg}")
+    else:
+        print("what")
+    
 if __name__ == "__main__":
     try:
-        print(yellow("---- running healthcheck... ----"))
+        log("running healthchck...", 2)
         healtcheck()
-        print(green("---- healthcheck successful ----"))
-        
-        print(yellow("---- running login... ----"))
+        log("healthcheck successful", 0)
+
+        log("running login...", 2)
         cookies = login()
-        print(green("---- login successful ----"))
-        print(green(cookies))
-        
-        print(yellow("---- running follow... ----"))
+        log("login successful", 0)
+
+        log("running follow...", 2)
         follow(cookies)
-        print(green("---- follow successful ----"))
+        log("follow successful", 0)
 
+        log("running unfollow...", 2)
+        unfollow(cookies)
+        log("unfollow successful", 0)
 
+        log("running get_followers...", 2)
+        get_followers(cookies)
+        log("get_followers successful", 0)
+
+        log("running get_following...", 2)
+        get_following(cookies)
+        log("get_following successful", 0)
 
     except Exception as e:
-        print(red("---- error ----"))
-        print(red(traceback.format_exc()))
-        print(red(f"testsuite ran into an error: {e}"))
+        log("An error occured", 1)
+        log(f"testsuite ran into an error: {e}", 1)
