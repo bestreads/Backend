93b0e6987068da1176aa779422f1b4874a943cec
erik <erschmia@gmail.com>
2026-02-11 14:16:50 +0100

    funktionierendes test-skript

--- testing/e2e.py
+++ testing/e2e.py
@@ -1,47 +1,73 @@
 import http.client as cl
 import json
+import traceback
+import requests
 
 HOST = "localhost:3000"
 API_PATH = "/api/v1"
 USERNAME = "test3@test.arpa"
 PASSWORD = "test1234567890"
 
-def healtcheck(conn: HTTPConnection):
-    conn.request(method="GET", url=f"{API_PATH}/healthcheck", headers={"Host": HOST})
-    res = conn.getresponse()
+def healtcheck():
+    resp = requests.get(url=f"http://{HOST}{API_PATH}/health")
 
-    if res.status != 200:
-        # uhhh idk wieso das so ist
-        # raise Exception("failed the healthcheck")
-        pass
-    else:
-        pass
+    if resp.status_code != 200:
+        raise Exception(f"failed the healthcheck: {resp.status_code}; body: {resp.text}")
+    return
 
-def login(conn: HTTPConnection) -> List[str]:
+def login() -> List[dict]:
+    resp = requests.post(url=f"http://{HOST}{API_PATH}/auth/login", json={"email": USERNAME, "password": PASSWORD})
+    tokens = {}
+    
+    if resp.status_code != 200:
+        raise Exception(f"failed to login, code {res.status}, msg: {body}")
 
-    body = json.dumps({"email": USERNAME, "password": PASSWORD})
-    conn.request(method="POST", url=f"{API_PATH}/auth/login", body=body, headers={"Host": HOST, "Content-Type": "application/json"})
-    res = conn.getresponse()
 
-    if res.status != 200:
-        raise Exception(f"failed to login, code {res.status}, msg: {res.read()}")
-
-    tokens = []
-    for h in res.getheaders():
-        if h[0] == "Set-Cookie":
-            tokens.append(h[1])
-
+    for h in resp.headers:
+        if h == "Set-Cookie":
+            tokens['Cookie']  = resp.headers[h]
+            
     return tokens
     
+def follow(tokens: dict[str]):
+    headers = {"Host": HOST}
+    headers['Cookie'] = tokens['Cookie']
+
+    resp = requests.post(url=f"http://{HOST}{API_PATH}/follow?id=10", headers=headers)
+
+    if resp.status_code != 200:
+        raise Exception(f"failed to follow user: {resp.status_code}; body: {resp.text}")
+    return
 
 
 
+def green(s: str) -> str:
+    return f"\033[1;49;32m{s}\033[0m"
+
+def red(s: str) -> str:
+    return f"\033[1;49;91m{s}\033[0m"
+
+def yellow(s: str) -> str:
+    return f"\033[1;49;33m{s}\033[0m"
 
 if __name__ == "__main__":
-    conn = cl.HTTPConnection(HOST)
     try:
-        healtcheck(conn)
-        cookies = login(conn)
+        print(yellow("---- running healthcheck... ----"))
+        healtcheck()
+        print(green("---- healthcheck successful ----"))
+        
+        print(yellow("---- running login... ----"))
+        cookies = login()
+        print(green("---- login successful ----"))
+        print(green(cookies))
+        
+        print(yellow("---- running follow... ----"))
+        follow(cookies)
+        print(green("---- follow successful ----"))
+
+
+
     except Exception as e:
-        print(e)
-        # print(e.args)
+        print(red("---- error ----"))
+        print(red(traceback.format_exc()))
+        print(red(f"testsuite ran into an error: {e}"))
