cad5b9ebc5a7d5a85674e878a6a1586cc6e5c81f
erik <erschmia@gmail.com>
2026-02-11 14:15:39 +0100

    struct tag fix
    warum ist dieser error *silent*???

--- internal/database/follow.go
+++ internal/database/follow.go
@@ -3,8 +3,8 @@ package database
 type FollowRel struct {
 	User User
 	// der user, der folgt
-	UserID    uint `gorm:"not null;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;uniqueIndex:idx_follow`
+	UserID    uint `gorm:"not null;constraint:OnUpdate:CASCATE,OnDelete:CASCATE;uniqueIndex:idx_follow"`
 	Following User
 	// der gefolgte user
-	FollowingID uint `gorm:"not null;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;uniqueIndex:idx_follow`
+	FollowingID uint `gorm:"not null;constraint:OnUpdate:CASCADE,OnDelete:CASCADE;uniqueIndex:idx_follow"`
 }
