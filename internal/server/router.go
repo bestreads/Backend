d2ca92f36634c641ea555edbea4a52e53cde9eba
Luca Schmitz <lschmitz@telekom.de>
2026-02-12 13:11:16 +0100

    Added route

--- internal/server/router.go
+++ internal/server/router.go
@@ -64,6 +64,7 @@ func setRoutes(cfg *config.Config, log zerolog.Logger, app *fiber.App) {
 	v1userProtected.Get("/", handlers.GetOwnUser)
 	v1userProtected.Put("/", handlers.ChangeUserData)
 	v1userProtected.Get("/:id", handlers.GetUserProfile)
+	v1userProtected.Delete("/", handlers.DeleteUser)
 
 	// library
 	// ?limit=n
