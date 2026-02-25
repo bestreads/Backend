17be5c15e870c5b2fc352cac8a64edabff3b254c
Luca Schmitz <lschmitz@telekom.de>
2026-02-25 15:21:02 +0100

    Add GetFollowingPostsFeed and translation function for post responses

---
+++ internal/services/following_posts.go
@@ -0,0 +1,41 @@
+package services
+
+import (
+	"context"
+
+	"github.com/bestreads/Backend/internal/database"
+	"github.com/bestreads/Backend/internal/dtos"
+	"github.com/bestreads/Backend/internal/repositories"
+)
+
+func GetFollowingPostsFeed(ctx context.Context, userId uint, offset int) ([]dtos.PostResponse, error) {
+	posts, getFollowingPostsErr := repositories.GetFollowingPostsFeed(ctx, userId, offset)
+	if getFollowingPostsErr != nil {
+		return nil, getFollowingPostsErr
+	}
+
+	postResponses, translatePostsErr := translatePostsToPostResponses(posts)
+	if translatePostsErr != nil {
+		return nil, translatePostsErr
+	}
+
+	return postResponses, nil
+}
+
+func translatePostsToPostResponses(posts []database.Post) ([]dtos.PostResponse, error) {
+	res := make([]dtos.PostResponse, len(posts))
+	for i, post := range posts {
+		res[i] = dtos.PostResponse{
+			ProfilePicture: post.User.ProfilePicture,
+			Username:       post.User.Username,
+			Uid:            post.User.ID,
+			Book:           post.Book,
+			Content:        post.Content,
+			CreatedAt:      post.UpdatedAt,
+			State:          post.State,
+			Rating:         post.Rating,
+		}
+	}
+
+	return res, nil
+}
